b6edfc0693012fef13752e93e823bf3e30f61024
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-06-09 23:14:19 +0530

    Add files via upload

---
+++ Output 3.py
@@ -0,0 +1,102 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:/Manish/New folder (2)/Output.py =================
+Enter your Number:32
+Binary equivalent: 0b100000 
+
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 2
+Enter first number: 32
+Enter second number: -56
+32.0 - -56.0 = 88.0
+Let's do next calculation? (yes/no): 3
+Enter choice(1/2/3/4): 32
+Invalid Input
+Enter choice(1/2/3/4): 3
+Enter first number: 32
+Enter second number: -56
+32.0 * -56.0 = -1792.0
+Let's do next calculation? (yes/no): e
+Enter choice(1/2/3/4): 4
+Enter first number: 32
+Enter second number: -56
+32.0 / -56.0 = -0.5714285714285714
+Let's do next calculation? (yes/no): no
+a)
+35 
+
+b)
+Enter the value of n:10
+45.0 
+
+c)
+Enter the value of radius:20
+5026.548245743669 
+
+d)
+Enter the value of r:10
+Enter the value of a:90
+Enter the value of b:90
+5.0 
+
+e)
+Enter Number y1:32
+Enter Number y2:45
+Enter Number x1:65
+Enter Number x2:21
+-0.29545454545454547 
+
+a part
+0 
+
+1 
+
+2 
+
+3 
+
+4 
+
+b part
+3 
+
+4 
+
+5 
+
+6 
+
+7 
+
+8 
+
+9 
+
+c part
+4 
+
+7 
+
+10 
+
+d part
+15 
+
+13 
+
+11 
+
+9 
+
+7 
+
+e part
+Enter Number of Hydrogen atoms:12
+Enter Number of Carbon atoms:23
+Enter Number of Oxygen atoms:11
+The total combined molecular weight in grams per mole is 464
